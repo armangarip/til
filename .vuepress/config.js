1ddb7e5ebe8fab67a30beb8e7974d24c04592ccd
Hasan Tayyar BEŞİK <tayyar.besik@gmail.com>
2018-05-02 13:55:12 +0200

    config update

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,3 +1,8 @@
-{
-	base: '/til/'
-}
+module.exports = {
+  base: '/til/',
+   head: [
+    ['meta', { name: 'theme-color', content: '#3eaf7c' }],
+    ['meta', { name: 'msapplication-TileColor', content: '#000000' }]
+  ]
+};
+
