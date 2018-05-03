8e426376688ace2a977c844192008b824abd7a53
Hasan Tayyar BEŞİK <tayyar.besik@gmail.com>
2018-05-03 22:29:33 +0200

    adam gibi navigasyon

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,6 +1,18 @@
 module.exports = {
   base: '/til/',
   title: '#',
+  themeConfig: {
+    sidebar: [
+      [ '/', 'Giris'],
+      {
+        title: '2018',
+        collapsable: true,
+        children: [
+          ['/a/entries/2018/May-2018/', 'May 2018']
+        ]
+      }
+    ]
+  },
   homepage: true,
    head: [
     ['meta', { name: 'theme-color', content: '#3eaf7c' }],
