4a81e7a61e752eae26eecbc3c091215cd65263d7
Hasan Tayyar BEŞİK <tayyar.besik@gmail.com>
2018-05-03 22:20:32 +0200

    bir link olsun azindan

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,5 +1,7 @@
 module.exports = {
   base: '/til/',
+  title: '#',
+  homepage: true,
    head: [
     ['meta', { name: 'theme-color', content: '#3eaf7c' }],
     ['meta', { name: 'msapplication-TileColor', content: '#000000' }]
